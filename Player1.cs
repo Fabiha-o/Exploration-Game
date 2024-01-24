9a14970e327ab609feacfd88fa6eead17126e8b3
Fabiha-o <123418104+Fabiha-o@users.noreply.github.com>
2024-01-24 17:58:59 -0500

    Changes to previous code

---
+++ Player1.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public int playerHealth;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+         Debug.Log("Hello Outsider");
+       
+       
+    }
+
+    void UseVector2()
+   {
+        Vector2 position = new Vector2(3.0f,2.0f);
+   }
+
+   void UseStaticProperties()
+   {
+        Vector2 moveLeft = Vector2.left;
+        Vector2 moveRight = Vector2.right;
+        Vector2 moveUp = Vector2.up;
+   }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Game Logic
+        
+        
+    }
+
+    void Awake()
+    {
+        playerHealth = 100;
+    }
+
+    void FixedUpdate()
+    {
+        //Physics realted code
+
+    }
+}
