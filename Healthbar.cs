8988de1b1542d7a858b3ff6123d69bd2295f32f9
Fabiha-o <123418104+Fabiha-o@users.noreply.github.com>
2024-03-25 18:08:58 -0400

    Add files via upload

---
+++ Healthbar.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Image _healthbarSprite;
+    public void UpdateHealthBar(float maxHealth, float currentHealth)
+    {
+        
+    }
+}
